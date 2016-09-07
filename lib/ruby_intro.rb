f09a062613943fe0f0285e1a7251801f1c3f64d3
Preston <powens1@citadel.edu>
2016-09-07 02:28:43 +0000

    update

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -66,7 +66,54 @@ def starts_with_consonant?(s)
 end
 
 def binary_multiple_of_4? s
-  # YOUR CODE HERE
+	#check if string is empty
+	if s.length == 0
+		return false
+	#string is not empty
+	else
+		#check that s only contains numbers
+		if (s !~ /\D/)
+			#counter for how many numbers are not 0 or 1
+			counter = 0
+			#run through each element of s
+			s.each_char do |x|
+				case x
+				when "0","1"
+					#contains 0 or 1
+				else
+					counter = counter + 1
+				end				
+			end
+			
+			#check if any non 0's or 1's where found
+			if counter > 0
+				return false
+			#do not look for alternative, end if statement
+			else
+				#reverse string
+				#makes it easier to get last numbers for me
+				string_reverse = s.reverse
+				first = string_reverse[0].to_i
+				second = string_reverse[1].to_i
+
+				#check that number is a multiple of four
+				#checks that lasts two, now first two, numbers are both zero
+				if(first == 0 && second == 0)
+					return true
+				#if first two numbers are not zero number is not divisible by four
+				else
+					return false
+				end
+			#end the search for non 0's and 1's
+			end
+		#s is not just numbers
+		else
+			return false
+		#end the search for different characters
+		end
+	#end the search for empty string	
+	end
+#end of definition  
 end
 
 # Part 3
